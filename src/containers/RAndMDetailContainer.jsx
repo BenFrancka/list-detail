3d06e75f70fba9e190cd701c74c121dd008c7867
BenFrancka <benfrancka@gmail.com>
2021-09-09 08:41:13 -0700

    adds detail container

---
+++ src/containers/RAndMDetailContainer.jsx
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import { fetchCharacterById } from '../services/RAndMApi';
+import CharacterDetail from '../components/CharacterDetail';
+
+export default class RAndMDetailContainer extends Component {
+  state = {
+    loading: true,
+    character: {},
+  };
+
+  async componentDidMount() {
+    const character = await fetchCharacterById();
+    this.setState({ character, loading: false });
+  }
+
+  render() {
+    const { character, loading } = this.state;
+    if(loading) {
+      return (
+        <img
+          src="https://thumbs.gfycat.com/SoftEarnestGyrfalcon-max-1mb.gif"
+          alt="loading spinner"
+        />
+      );
+    }
+    return <CharacterDetail character={character} />;
+  }
+}
--- src/services/RAndMApi.js
+++ src/services/RAndMApi.js
@@ -8,3 +8,17 @@ export const fetchCharacters = async () => {
     image: character.image,
   }));
 };
+
+export const fetchCharacterById = async () => {
+  const id = params.id;
+  const res = await fetch(`https://rickandmortyapi.com/api/character/${id}`);
+  const json = await res.json();
+
+  return json.results.map((character) => ({
+    id: character.id,
+    name: character.name,
+    image: character.image,
+    status: character.status,
+    species: character.species
+  }));
+};
